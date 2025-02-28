162f84c9a66af5e42d5f0b2396aa48f3ba1235d7
Robledo Castro <bedocastro@gmail.com>
2025-02-28 19:58:39 -0300

    Projeto cursomc1 criado

---
+++ src/main/java/com/robledocastro/cursomc1/resources/CategoriaResource.java
@@ -0,0 +1,30 @@
+package com.robledocastro.cursomc1.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.robledocastro.cursomc1.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+	
+	
+}

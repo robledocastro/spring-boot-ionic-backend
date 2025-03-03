8e7eae45c6537415c6d1756006998f0f93528666
Robledo Castro <bedocastro@gmail.com>
2025-03-03 11:41:50 -0300

    Criando Estado e Cidade

--- src/main/java/com/robledocastro/cursomc1/Cursomc1Application.java
+++ src/main/java/com/robledocastro/cursomc1/Cursomc1Application.java
@@ -1,6 +1,5 @@
 package com.robledocastro.cursomc1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.robledocastro.cursomc1.domain.Categoria;
+import com.robledocastro.cursomc1.domain.Cidade;
+import com.robledocastro.cursomc1.domain.Estado;
 import com.robledocastro.cursomc1.domain.Produto;
 import com.robledocastro.cursomc1.repositories.CategoriaRepository;
+import com.robledocastro.cursomc1.repositories.CidadeRepository;
+import com.robledocastro.cursomc1.repositories.EstadoRepository;
 import com.robledocastro.cursomc1.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,7 +22,12 @@ public class Cursomc1Application implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
-	private ProdutoRepository produtoRepository; 
+	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc1Application.class, args);
@@ -44,6 +52,20 @@ public class Cursomc1Application implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 	
---
+++ src/main/java/com/robledocastro/cursomc1/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.robledocastro.cursomc1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.robledocastro.cursomc1.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}

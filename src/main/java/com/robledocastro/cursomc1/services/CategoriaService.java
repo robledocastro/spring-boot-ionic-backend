1d2521cb5c033fe34d367c0059abee7d6b1ed07d
Robledo Castro <bedocastro@gmail.com>
2025-03-05 22:06:46 -0300

    Deletando uma categoria com DELETE e criando excecao personalizada

--- src/main/java/com/robledocastro/cursomc1/services/CategoriaService.java
+++ src/main/java/com/robledocastro/cursomc1/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.robledocastro.cursomc1.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.robledocastro.cursomc1.domain.Categoria;
 import com.robledocastro.cursomc1.repositories.CategoriaRepository;
+import com.robledocastro.cursomc1.services.exceptions.DataIntegrityException;
 import com.robledocastro.cursomc1.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -17,7 +19,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 return obj.orElseThrow(() -> new ObjectNotFoundException(
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
 	
@@ -30,4 +32,16 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);			
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
+	}
+	
+	
 }

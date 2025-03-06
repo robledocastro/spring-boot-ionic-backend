63831a34f5d976d947d23b7e381ba04a8328614d
Robledo Castro <bedocastro@gmail.com>
2025-03-06 12:17:11 -0300

    Listando todas categorias com DTO

--- src/main/java/com/robledocastro/cursomc1/resources/CategoriaResource.java
+++ src/main/java/com/robledocastro/cursomc1/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.robledocastro.cursomc1.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.robledocastro.cursomc1.domain.Categoria;
+import com.robledocastro.cursomc1.dto.CategoriaDTO;
 import com.robledocastro.cursomc1.services.CategoriaService;
 
 @RestController
@@ -47,4 +50,12 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
 }
--- src/main/java/com/robledocastro/cursomc1/services/CategoriaService.java
+++ src/main/java/com/robledocastro/cursomc1/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.robledocastro.cursomc1.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,8 @@ public class CategoriaService {
 		}
 	}
 	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 	
 }
